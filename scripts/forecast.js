df3a431a6ee289a61c68ff6c202e6dacfbf9f5a9
Coltin <coltinrb@gmail.com>
2022-04-01 11:30:46 -0500

    initial commit

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+const key = 'gLFpDLnlJg46isO27Iu4tnsL4WJ2WcrM';
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+
